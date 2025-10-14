5328fb328675f654b42ab1c141f07a9bf5c96299
fidel <fideljose1708@hotmail.com>
2025-10-14 12:16:41 -0500

    Use InMemoryUserDetailsManager - Credentials

--- src/main/java/com/fideljose/testSpringSecurity/config/Configurations.java
+++ src/main/java/com/fideljose/testSpringSecurity/config/Configurations.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -24,6 +28,23 @@ public class Configurations {
         //http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService  userDetailsService() {
+        UserDetails users = User.builder()
+                .username("user1")
+                .password("{noop}password1")
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.builder()
+                .username("admin1")
+                .password("{noop}passwordAdmin1")
+                .roles("USER", "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(users, admin);
     }
 
 }
